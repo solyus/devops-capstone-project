2dc996319651e862f18a458c3b6f835cfffe495b
solyus <141231224+solyus@users.noreply.github.com>
2024-02-04 13:42:25 +0100

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -46,6 +46,16 @@ class TestAccountService(TestCase):
 
         self.client = app.test_client()
 
+      def test_get_account(self):
+        """It should Read a single Account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.get(
+            f"{BASE_URL}/{account.id}", content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data["name"], account.name)
+
     def tearDown(self):
         """Runs once after each test case"""
         db.session.remove()
